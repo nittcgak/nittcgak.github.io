ec0975567fa621508d6921aa95c7a65ddade8b62
i20hirota <i20tokuyama.kosen-ac.jp>
2023-10-01 21:23:26 +0900

    ボタンの追加

--- js/app.js
+++ js/app.js
@@ -1,34 +1,24 @@
 (() => {
 
   const $button = document.getElementsByClassName('buttons');
-  const $top = document.getElementById('js-top');
-  const $mogi = document.getElementById('js-mogi');
-  const $flyer = document.getElementById('js-flyer');
-  const $scheMap = document.getElementById('js-scheMap');
+  const $main = document.getElementById('js-main');
+  console.log($main);
+  const $content = $main.querySelectorAll('[data-content]');
+  console.log($content);
 
-
+  //初期化
   const init = () => {
-    $top.classList.toggle("hidden");
+    $content[0].classList.remove("hidden");
   };
   init();
 
-  // for (let i = 0; i < $button.length; i++) {
-  //   $button[i].addEventListener('click', function () {
-  //     $top.classList.toggle("hidden");
-  //   });
-  // }
-
-  $button[0].addEventListener('click', function () {
-    $top.classList.toggle("hidden");
-  });
-  $button[1].addEventListener('click', function () {
-    $mogi.classList.toggle("hidden");
-  });
-  $button[2].addEventListener('click', function () {
-    $flyer.classList.toggle("hidden");
-  });
-  $button[3].addEventListener('click', function () {
-    $scheMap.classList.toggle("hidden");
-  });
+  for (let i = 0; i < $button.length; i++) {
+    $button[i].addEventListener('click', function () {
+      for(let j=0;j<$button.length;j++){
+        $content[j].classList.add("hidden");
+      }
+      $content[i].classList.remove("hidden");
+    });
+  }
 
 })();
