d37962d90e9b66c8f108b057d1c951cefd329842
i20hirota <i20tokuyama.kosen-ac.jp>
2023-10-02 18:31:28 +0900

    大きく変更

--- js/button.js
+++ js/button.js
@@ -2,11 +2,14 @@
 
   const $button = document.getElementsByClassName('buttons');
   const $main = document.getElementById('js-main');
+  const $space = document.getElementById('header-space');
   const $content = $main.querySelectorAll('[data-content]');
 
   //初期化
   const init = () => {
     $content[0].classList.remove("hidden");
+    $button[0].classList.add("button-active");
+    $space.classList.add("hidden");
   };
   init();
 
@@ -14,8 +17,15 @@
     $button[i].addEventListener('click', function () {
       for(let j=0;j<$button.length;j++){
         $content[j].classList.add("hidden");
+        $button[j].classList.remove("button-active");
+      }
+      if(i===0){
+        $space.classList.add("hidden");
+      }else{
+        $space.classList.remove("hidden");
       }
       $content[i].classList.remove("hidden");
+      $button[i].classList.add("button-active");
     });
   }
 
